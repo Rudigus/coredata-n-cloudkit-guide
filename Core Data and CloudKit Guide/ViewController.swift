1ac65e0331742c43222314aeeaaeee3156df0664
Rudigus <rodrigo.matos9@hotmail.com>
2020-10-19 09:37:15 -0300

    initial project files

---
+++ Core Data and CloudKit Guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Data and CloudKit Guide
+//
+//  Created by Rodrigo Matos Aguiar on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
